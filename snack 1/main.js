50a2db345948e9239ab8f3f357d9c85149e62dca
Aribunfi <arianna@pabis.de>
2023-01-30 17:00:21 +0100

    mille tentativi falliti

--- snack 1/main.js
+++ snack 1/main.js
@@ -1,3 +1,19 @@
 // Snack1
 // Ottieni un numero casuale in un range specificato.
 // Chiedi all'utente il valore minimo ed il valore massimo del range in cui generare il numero.
+
+let minNumber = prompt("Scrivi il numero minore");
+let maxNumber = prompt("Scrivi il numero maggiore");
+
+
+
+
+// function getRandomNumber (min, max) {
+//     let step1 = max - min + 1;
+//     let step2 = Math.random() * step1;
+//     let result = Math.floor(step2) + min;
+
+      
+// }
+
+console.log(result);
